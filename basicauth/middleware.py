b8c804b83bc2afe646cff642690e5d3906b8cbc9
Devin <gnujava@gmail.com>
2017-07-20 13:06:21 +1200

    Added supporting for django 1.11

--- basicauth/middleware.py
+++ basicauth/middleware.py
@@ -1,8 +1,9 @@
 from .basicauthutils import validate_request
 from .response import HttpResponseUnauthorized
+from django.utils.deprecation import MiddlewareMixin
 
 
-class BasicAuthMiddleware:
+class BasicAuthMiddleware(MiddlewareMixin):
     def process_request(self, request):
         validated_username = validate_request(request)
         if validated_username is None:
